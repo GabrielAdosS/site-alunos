d3f45a1f22ce7eb502b65602fcc828262f5ce92f
Gabriel A <gabriel.17.set.2005@gmail.com>
2025-04-18 11:45:09 -0300

    Criando componentes, servidor json e services

---
+++ src/app/pages/cadastrar/cadastrar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../../footer/footer.component';
+
+@Component({
+  selector: 'app-cadastrar',
+  imports: [FooterComponent],
+  templateUrl: './cadastrar.component.html',
+  styleUrl: './cadastrar.component.css'
+})
+export class CadastrarComponent {
+
+}
---
+++ src/app/pages/listagem/listagem.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../../footer/footer.component';
+
+@Component({
+  selector: 'app-listagem',
+  imports: [FooterComponent],
+  templateUrl: './listagem.component.html',
+  styleUrl: './listagem.component.css'
+})
+export class ListagemComponent {
+
+}
